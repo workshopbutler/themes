72faf8ea2efd40c8cbd2c0134f5a4411068bb2fb
Sergey Kotlov <skotlov@gmail.com>
2018-06-14 18:12:42 +0300

    Fixes a bug on a separate registration page and updates widgets version to 0.5.0

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const isDev = (process.env.NODE_ENV !== "build");
 
 const options = {
     apiKey: process.env.API_KEY,
-    widgetVersion: '0.4.0',
+    widgetVersion: '0.5.0',
     theme: 'hayes'
 };
 
